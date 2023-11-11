9e9c654e790aa78808295bd803197450aa89b539
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-11 13:31:02 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,27 +1,36 @@
 import requests
+import argparse
+import pandas as pd
+from openpyxl import Workbook
+
+def generate_excel():
+    return 0
+    # return new excel
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Client for the task")
+parser.add_argument("-k", "--keys", nargs='+', help="Additional columns to consider")
+parser.add_argument("-c", "--colored", action="store_true", help="Enable row coloring")
+args = parser.parse_args()
+
+df = pd.read_csv("vehicles.csv")
+
 
 url = "http://localhost:8000/upload-excel"  # Replace with your FastAPI server's URL, i put my local host
 file_path = "vehicles.csv"  
 
-files = {"file": open(file_path, "rb")}
+files = {"file": ("vehicles.csv", open("vehicles.csv", "rb"))}
 response = requests.post(url, files=files)
 
 if response.status_code == 200:
-    print("File successfully uploaded to the server.")
+    data = response.json()
+
+    # Process the JSON data and generate the Excel file as per the requirements
+    generate_excel(data, args.keys, args.colored)
 else:
     print(f"Failed to upload the file. Status code: {response.status_code}")
 
-url = "https://api.baubuddy.de/index.php/login"
-payload = {
-    "username": "365",
-    "password": "1"
-}
-headers = {
-    "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
-    "Content-Type": "application/json"
-}
-response = requests.request("POST", url, json=payload, headers=headers)
-temp=response.text.split('"')
-print(temp)
+
+
 
 
