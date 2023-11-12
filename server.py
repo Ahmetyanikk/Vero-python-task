6adcb8d0bebeebdceab9a10489e124f2f273e3c5
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-12 22:23:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-
+import requests
 
 app = FastAPI()
+url = "https://api.baubuddy.de/index.php/login"
+payload = {
+    "username": "365",
+    "password": "1"
+}
+headers = {
+    "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+    "Content-Type": "application/json"
+}
+response = requests.request("POST", url, json=payload, headers=headers)
+temp=response.text.split('"')
+print(temp[5])
+
 
 @app.get("/")
 async def read_root():
