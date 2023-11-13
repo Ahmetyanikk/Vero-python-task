e8ae91ce6044e9f8c42e53063bc6e24c4d095cf3
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-14 00:50:03 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,29 +4,29 @@ import pandas as pd
 from openpyxl import Workbook
 
 
-
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Client for the task")
 parser.add_argument("-k", "--keys", nargs='+', help="Additional columns to consider")
 parser.add_argument("-c", "--colored", action="store_true", help="Enable row coloring")
 args = parser.parse_args()
-
-df = pd.read_csv("vehicles.csv")
+print(args)
+data = {
+    'keys': 'None',
+    'colored': 'True'
+}
 
 
 url = "http://localhost:8000/upload-excel"  # Replace with your FastAPI server's URL, i put my local host
-file_path = "vehicles.csv"  
+file_path = r"C:\Users\Ahmet\PycharmProjects\untitled8\vehicles.csv"
 
 files = {"file": ("vehicles.csv", open("vehicles.csv", "rb"))}
-response = requests.post(url, files=files)
-
+response = requests.post(url,data=data)
+print("response.text")
 if response.status_code == 200:
-    data = response.json()
-
-
+    print(response)
+    # Process the JSON data and generate the Excel file as per the requirements
 else:
     print(f"Failed to upload the file. Status code: {response.status_code}")
-
 
 
 
