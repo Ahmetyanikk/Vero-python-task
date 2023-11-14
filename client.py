6e982554cb8b2a2af55b390a9d8e51c5c101d717
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-15 00:59:21 +0300

    Update client.py

--- client.py
+++ client.py
@@ -6,24 +6,23 @@ from openpyxl import Workbook
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Client for the task")
-parser.add_argument("-k", "--keys", nargs='+', help="Additional columns to consider")
-parser.add_argument("-c", "--colored", action="store_true", help="Enable row coloring")
+parser.add_argument("-k", "--keys", type=str,default='',action='store',nargs='+', help="Additional columns to consider")
+parser.add_argument("-c", "--colored", action="store_false", help="Enable row coloring")
 args = parser.parse_args()
-print(args)
 data = {
-    'keys': 'None',
-    'colored': 'True'
+    'keys': args.keys,
+    'colored': args.colored
 }
+print(type('keys'))
 
 
 url = "http://localhost:8000/upload-excel"  # Replace with your FastAPI server's URL, i put my local host
 file_path = r"C:\Users\Ahmet\PycharmProjects\untitled8\vehicles.csv"
 
 files = {"file": ("vehicles.csv", open("vehicles.csv", "rb"))}
-response = requests.post(url,data=data)
-print("response.text")
+response = requests.post(url,files=files,data=data)
 if response.status_code == 200:
-    print(response)
+    print(response.text)
     # Process the JSON data and generate the Excel file as per the requirements
 else:
     print(f"Failed to upload the file. Status code: {response.status_code}")
