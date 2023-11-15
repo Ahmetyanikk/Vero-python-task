140d070698c1431eb79c4cab8f64cad4d56fa08b
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-16 00:30:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,Form,Body
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
 import requests
 from pathlib import Path
-from pydantic import BaseModel
+from pydantic import BaseModel, conint, model_validator
+from typing import Annotated
+import json
 
 
 
@@ -21,9 +23,9 @@ response = requests.request("POST", url, json=payload, headers=headers)
 temp=response.text.split('"')
 print(temp[5])
 
-
-
-
+class Item(BaseModel):
+    keys: str
+    colored: bool
 
 @app.get("/")
 async def read_root():
@@ -31,11 +33,11 @@ async def read_root():
 
 
 @app.post("/upload-excel")
-async def upload_excel(file: UploadFile ):
+async def upload_excel(file: UploadFile = Form(...), json_data: str = Form(...)):
+    contents = await file.read()
 
+    return {"file_name": file.filename, "file_size": len(contents), "json_data": json_data}
 
-
-    return {"file_name": file.filename}
 
 
 
