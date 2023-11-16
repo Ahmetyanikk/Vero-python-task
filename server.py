13e6098d6c4558a6f71f46e7791d0a64fb6c52cc
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-16 23:52:36 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from pydantic import BaseModel, conint, model_validator
 from typing import Annotated
 import json
+import pandas as pd
 
 
 
@@ -23,9 +24,13 @@ response = requests.request("POST", url, json=payload, headers=headers)
 temp=response.text.split('"')
 print(temp[5])
 
-class Item(BaseModel):
-    keys: str
-    colored: bool
+def save_csv(file_contents: bytes, destination: Path):
+    with open(destination, "wb") as file_object:
+        file_object.write(file_contents)
+
+
+
+
 
 @app.get("/")
 async def read_root():
@@ -33,10 +38,32 @@ async def read_root():
 
 
 @app.post("/upload-excel")
-async def upload_excel(file: UploadFile = Form(...), json_data: str = Form(...)):
+async def upload_excel(file: UploadFile):
     contents = await file.read()
 
-    return {"file_name": file.filename, "file_size": len(contents), "json_data": json_data}
+    # Specify the destination path to save the CSV file
+    destination_path = Path(r"C:\Users\Ahmet\PycharmProjects\pythonProject1\Serverfiles") / file.filename
+
+    # Save the CSV file
+    save_csv(contents, destination_path)
+
+
+    return {"File sent"}
+
+@app.post("/Format-excel")
+async def upload_excel(item: dict):
+    Dfcsv = pd.read_excel(r'C:\Users\Ahmet\PycharmProjects\pythonProject1\Serverfiles\vehicles.csv')
+    colored= True
+    if colored == True:
+        try:
+            for i in range(0, len(Dfcsv['hu'])):
+                print("asd")
+        except:
+            print("error")
+
+
+
+    return {"File sent"}
 
 
 
