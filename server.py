7eeef32f356d37f81b3420f11293bdfc104f4c15
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2023-11-17 14:37:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,18 @@ from pydantic import BaseModel, conint, model_validator
 from typing import Annotated
 import json
 import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill, Border, Side
+from datetime import datetime
 
+def color_rows(row):
+    today = datetime.today()
+    if (today - row['hu']).days <= 90:
+        return ['background-color: #007500'] * len(row)
+    elif (today - row['hu']).days <= 365:
+        return ['background-color: #FFA500'] * len(row)
+    else:
+        return ['background-color: #b30000'] * len(row)
 
 
 app = FastAPI()
@@ -53,18 +64,20 @@ async def upload_excel(file: UploadFile):
 @app.post("/Format-excel")
 async def upload_excel(item: dict):
     Dfcsv = pd.read_excel(r'C:\Users\Ahmet\PycharmProjects\pythonProject1\Serverfiles\vehicles.csv')
+    Dfcsv.sort_values(by='gruppe',inplace=True)
     colored= True
     if colored == True:
         try:
-            for i in range(0, len(Dfcsv['hu'])):
-                print("asd")
+            styled_df = Dfcsv.style.apply(color_rows, axis=1)
+
         except:
             print("error")
-
+    excel_writer = pd.ExcelWriter("formatted_excel_with_conditions.xlsx", engine='openpyxl')
+    styled_df.to_excel(excel_writer, sheet_name='Sheet1', index=False)
+    excel_writer.save()
 
 
     return {"File sent"}
-
 
 
 
